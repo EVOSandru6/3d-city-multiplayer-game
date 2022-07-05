2772af16d149ec58bc10638cf29d50e8a3716eea
EVOSandru6 <evosduple@mail.ru>
2022-07-06 00:18:03 +0300

    release socket

--- socket-test/app.js
+++ socket-test/app.js
@@ -1,21 +1,30 @@
 const app = require('express')()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     // res.send('<h1>Hello</h1>')
     res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', function (socket) {
     console.log('User is connected')
+    socket.emit('hi');
 
     socket.on('disconnect', function () {
         console.log('User is disconnected')
     })
+
+    socket.on('chat message', function (msg) {
+        console.log(`msg:${msg}`)
+        socket.broadcast.emit('chat message', msg) // всем кроме излучающего
+        // socket.emit('chat message', msg) // всем
+    })
 })
 
 let port = 3000
-http.listen(port, function() {
+server.listen(port, function () {
     console.log(`Listening on port ${port}`)
 })
