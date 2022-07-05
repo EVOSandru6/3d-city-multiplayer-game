e50027c02632b00ea37f1162f2a3b939c7695597
EVOSandru6 <evosduple@mail.ru>
2022-07-05 23:47:22 +0300

    release socket io example

--- socket-test/app.js
+++ socket-test/app.js
@@ -1,9 +1,18 @@
 const app = require('express')()
 const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.get('/', function(req, res) {
     // res.send('<h1>Hello</h1>')
     res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', function (socket) {
+    console.log('User is connected')
+
+    socket.on('disconnect', function () {
+        console.log('User is disconnected')
+    })
 })
 
 let port = 3000
